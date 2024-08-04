5cf977b0b1b6639e5de7404cf940e761ff6871ce
limsiva <38914974+limsiva@users.noreply.github.com>
2024-08-04 19:02:18 -0300

    Update poti

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ var encontros = [
     {"title": "13. Apresentação do Produto/ação de Intervenção - 07/12/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_13.html"},
 ]
 
-for (i = 0; i<= encontros.length; i++) {
-    document.getElementById("encontros-list").innerHTML += "<li><a href='" + encontros[i].uri + "'>" + encontros[i].title + "</li>";
+window.onload = function() {
+
+    document.getElementById("foot-data").innerHTML += '<h5 style="font-family: Arial, sans-serif; margin: 20px 0 0; font-size: 11pt;">UNIVERSIDADE FEDERAL DO RIO GRANDE DO NORTE</h5><p>Portfólio de Atividade Interativa Interdisciplinar II: Saúde e Cidadania (SACI II) 2024.1</p><p>Docente: Ana Kalliny de Sousa Severo</p><p>Docente Assistida: Anna Cecilia de Medeiros Lira Câmara</p><p>Discente: Sivaldo Ponciano de Lima Junior</p>';
+
+    for (i = 0; i<= encontros.length; i++) {
+        document.getElementById("encontros-list").innerHTML += "<li><a href='" + encontros[i].uri + "'>" + encontros[i].title + "</li>";
+        
+    }
 }
+
