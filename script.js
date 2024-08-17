9f5f8ad2b32ddf46c9a26e3e01fd59899bef6c7c
limsiva <38914974+limsiva@users.noreply.github.com>
2024-08-16 21:21:33 -0300

    update dia_4.html

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var encontros = [
     {"title": "1. Aula Inaugural - 05/03/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_1.html"},
     {"title": "2. Apresentação do Componente Curricular - 12/03/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_2.html"},
     {"title": "3. Conhecendo a unidade de saúde da família - 19/03/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_3.html"},
+    {"title": "4. Atenção Primária à Saúde - 26/03/2024 ", "uri": "https://limsiva.github.io/poti/encontros/dia_4.html"},
     {"title": "6. Tenda do conto - 09/04/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_6.html"},
 ]
 
