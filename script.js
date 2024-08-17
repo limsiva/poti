ace58361f0b0e418ed5c3aa1b4d5c3302ef7f79e
limsiva <38914974+limsiva@users.noreply.github.com>
2024-08-16 21:24:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ var encontros = [
     {"title": "2. Apresentação do Componente Curricular - 12/03/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_2.html"},
     {"title": "3. Conhecendo a unidade de saúde da família - 19/03/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_3.html"},
     {"title": "4. Atenção Primária à Saúde - 26/03/2024 ", "uri": "https://limsiva.github.io/poti/encontros/dia_4.html"},
+    {"title": "5. Política Nacional de Atenção Básica - 02/04/2024 ", "uri": "https://limsiva.github.io/poti/encontros/dia_5.html"},
     {"title": "6. Tenda do conto - 09/04/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_6.html"},
+    {"title": "7. Sistemas de Informação na Atenção Básica - 23/07/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_7.html"},
+    {"title": "8. Ferramentas para o cuidado familiar - 30/07/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_8.html"},
+    {"title": "9. Visitas Domiciliares - 06/08/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_9.html"},
+    {"title": "10. A Saúde do Trabalhador e Sua Rede - 13/08/2024", "uri": "https://limsiva.github.io/poti/encontros/dia_10.html"},
 ]
 
 window.onload = function() {
